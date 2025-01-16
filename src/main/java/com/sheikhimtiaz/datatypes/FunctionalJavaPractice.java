a75baa57465191146ed107b46aa34669a66e2651
Sheikh Imtiaz Ahmed <imticorei54@gmail.com>
2025-01-16 06:55:16 +0600

    functional java

---
+++ src/main/java/com/sheikhimtiaz/datatypes/FunctionalJavaPractice.java
@@ -0,0 +1,17 @@
+package com.sheikhimtiaz.datatypes;
+
+public class FunctionalJavaPractice {
+    public static void run() {
+
+        int i = 7;
+
+//        IFuncInt funcInt = (num1, num2) ->
+//        {
+
+            // It produces an error due to effectively final
+            // variable not declared
+//            i = num1 + num2;
+//            return i;
+//        };
+    }
+}
---
+++ src/main/java/com/sheikhimtiaz/datatypes/IFuncInt.java
@@ -0,0 +1,7 @@
+package com.sheikhimtiaz.datatypes;
+
+@FunctionalInterface
+public interface IFuncInt {
+    int func(int num1, int num2);
+    public String toString();
+}
